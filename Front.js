748b0063facf87332329777e99b12d27270d055c
adrianbonuel <120789848+adrianbonuel@users.noreply.github.com>
2023-01-13 12:10:51 +0800

    Add files via upload

---
+++ Front.js
@@ -0,0 +1,6 @@
+
+let ok = confirm("sure kana talaga ha?");
+
+if (ok == false) {
+  document.getElementById("button").inerHTML="what if in pindot mo an ok";
+}
---
+++ Pau.js
@@ -0,0 +1,12 @@
+let ok = confirm("sure kana talaga ha?"); 
+
+
+
+alert("Hi paula hehe")
+alert("Na agihan mona an hikaw mo?")
+alert("Btw ingat ka po palagi 🤟")
+alert("And mag puyat an tama lang di parehas sa ako na 4AM na nag tuturog")
+alert("Tapos di magpa gutom kaon sa tama na oras🙂")
+alert("And goodluck sa college life kaya nato yadi☺️")
+
+
